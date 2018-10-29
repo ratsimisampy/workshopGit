5ecf618ea9d5588a8368680b86046267125dce8b
ratsimisampy <ratsimisampy@gmail.com>
2018-10-29 21:17:19 +0100

    routes up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # add comments 
-
+  # add new code for new branch feature1
 end
